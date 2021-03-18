ca69a5dc0127657d33c9fa41688636c2e700a51e
Kshitiz Saini <kshitizsaini.rtk@gmail.com>
2021-03-18 11:38:03 +0530

    Calling the functions

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,32 @@ int main()
 {
     vector <string> processId = ProcessParcer::getProcessIdList();
 
-    std::cout<<"PID\tRAM Size\tProcessUP\tSystemUP\tCPU Percent\tUser\tCommand"<<std::endl;
+    cout << "SYSTEM DETAILS" << endl;
+    cout << "----------------------------------------------------------------------------------------------------" << endl;
+    cout << "OS Installed: \t\t\t" + ProcessParcer::getOsName() << endl;
+    cout << "Kernel Version: \t\t" + ProcessParcer::getSystemKernelVersion() << endl;
+    cout << "Number of CPU Core: \t\t" + to_string(ProcessParcer::getNumberOfCores()) <<endl;
+    cout << "System Up Time: \t\t" + to_string(ProcessParcer::getSystemUpTime()) + " sec" << endl;
+    cout << "System Ram Usage: \t\t" + to_string(ProcessParcer::getSystemRamPercent()) + "%" << endl;
+    cout << "Total Processes: \t\t" + to_string(ProcessParcer::getTotalNumberOfProcesses()) << endl;
+    cout << "Total Threads: \t\t\t" + to_string(ProcessParcer::getTotalThreads()) << endl;
+    cout << "Total Running Process: \t\t" + to_string(ProcessParcer::getNumberOfRunningProcesses()) << endl;
 
-    for(int i=0; i<processId.size(); i++)
+    cout << "====================================================================================================" << endl;
+
+    cout << endl << endl << endl;
+
+    cout<<"PID\tRAM Size(GB)\tProcessUP(s)\tCPU Percent(%)\tUser"<<endl;
+    cout << "----------------------------------------------------------------------------------------------------" << endl;
+
+    for(int i=0; i<processId.size()-1; i++)
     {
-        std::cout<<processId.at(i)<<"\t";
+        cout<<processId.at(i)<<"\t";
+        cout<<ProcessParcer::getProcessRamPercent(processId.at(i))<<"\t";
+        cout<<ProcessParcer::getProcessUpTime(processId.at(i))<<"\t";
+        cout<<ProcessParcer::getProcessCpuPercent(processId.at(i))<<"\t";
+        cout<<ProcessParcer::getProcessUser(processId.at(i))<<endl;
+        // cout<<ProcessParcer::getProcessCommand(processId.at(i))<<endl<<endl;
     }
 
     return 0;
